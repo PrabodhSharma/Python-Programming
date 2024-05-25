3ded7a490b6692fef0460a3999beed813e1c5ed8
Prabodh Sharma <prabodhsharma247@gmail.com>
2024-05-25 18:02:27 -0400

    Advanced Data Types and Input/Output
    Added "Frequency Analysis" and "Readability Score"

--- Week 3/Text Analyzer Tool.py
+++ Week 3/Text Analyzer Tool.py
@@ -13,21 +13,23 @@ text = input("Enter your text: ")
 
 
 # Function to perform analysis feature
-def analysis():
+def analyze():
     global word_count, char_count_with_spaces, char_count_without_spaces, frequency_analysis, palindromes, readibility_score
 
-    # Word count
+    # WORD COUNT
     word_count = len(text.split())
     print("Word Count: ", word_count)
 
-    # Character count                                           
+
+    # CHARACTER COUNT                                           
     char_count_with_spaces = len(text)
     print("Character Count With Spaces: ", char_count_with_spaces)
 
     char_count_without_spaces = len(text.replace(" ", ""))
     print("Character Count Without Spaces: ", char_count_without_spaces)
 
-    # Palindrome check
+
+    # PALINDROME CHECK
     words = text.split()                                        # Split the input text into a list of words
     for word in words:                                          # Check if the current word is equal to its reverse
         if word == word[::-1]:
@@ -38,4 +40,31 @@ def analysis():
         print("Palindromes: ", palindromes)
     else:
         print("Palindromes: No Palindrome Found!")
-analysis()
+
+
+    # FREQUENCY ANALYSIS
+    for char in text:
+        # frequency_analysis.get(char, 0): This retrieves the current count of the character 'char' from the dictionary. If the character is not found in the dictionary, it returns 0.
+        # If the character is already in the dictionary, increment its count by 1, if not set it counts to 1
+        frequency_analysis[char] = frequency_analysis.get(char, 0) + 1
+
+    # Display frequency analysis                  
+    if frequency_analysis:
+        print("Frequency Analysis: ", frequency_analysis)
+    else:
+        print("Frequency Analysis: 0")
+
+
+    # READABILITY SCORE
+    total_word_lenght = 0
+    for word in text.split():
+        total_word_lenght += len(word)                          # Adds the length of the current word to the total word length
+    
+    if word_count != 0:                                         # Checks if the word count is not zero to avoid division by zero
+        readibility_score = total_word_lenght / word_count
+    else:
+        readibility_score = 0
+
+    print(f"Readability Score: {readibility_score: .2f}")       # Displays readability score with two decimal places
+
+analyze()
