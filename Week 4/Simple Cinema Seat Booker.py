3eda71f050b4188ae543a55f9c6c37adde7cb987
Prabodh Sharma <prabodhsharma247@gmail.com>
2024-06-12 14:57:13 -0400

    Decision structures, loops, nested loops and nested lists
    Initialize the seating arrangements

---
+++ Week 4/Simple Cinema Seat Booker.py
@@ -0,0 +1,3 @@
+
+# Initialize the seating arrangement
+seats = [[False] * 5 for _ in range(3)]                                     # initializes a nested list called seats. [[False] * 5 for _ in range(3)] creates a list with 3 sub-lists (representing rows). Each sub-list contains 5 False values (representing seats). 'False' indicates that the seat is not booked. 
