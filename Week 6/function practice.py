8cff86e95cd400882464d198e01683a0c927669c
Prabodh Sharma <prabodhsharma247@gmail.com>
2024-06-22 15:46:04 -0400

    Defining and using functions (void functions, value returning functions, and functions within functions)
    • Added a void function to display a menu.
    • Added a value-returning function to calculate the area of a rectangle.

---
+++ Week 6/function practice.py
@@ -0,0 +1,27 @@
+
+# 1. Void function to display a menu.
+def display_menu():
+
+    """
+    This function displays the menu options to the user.
+    """
+
+    print("Menu:")
+    print("1. Calculate the area of rectangle")
+    print("2. Check if a number is prime")
+    print("3. Exit")
+
+
+# 2. Value-returning function to calculate the area of rectangle.
+def calculate_area(length, width):
+
+    """
+    This function calculates the area of rectangle.
+    :parameter length: Length of rectangle
+    :parameter width: Width of rectangle
+    :return: Area of rectangle
+    """
+
+    return length * width
+
+
